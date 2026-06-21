cc356d86772a92356eef81ab4de059ab4f849694
Phillipi Garcia <164583791+phillipi-garcia@users.noreply.github.com>
2026-06-21 12:52:15 -0300

    Update SrpClasses.cs

--- src/Logistica.App/SRP/SrpClasses.cs
+++ src/Logistica.App/SRP/SrpClasses.cs
@@ -1,33 +1,25 @@
 using System;
 
-namespace Logistica.App.SRP
-{
-    public class Pedido
-    {
+namespace Logistica.App.SRP{
+    public class Pedido{
         public int Id { get; set; }
         public decimal Valor { get; set; }
     }
 
-    public class PedidoRepository
-    {
-        public void SalvarNoBanco(Pedido pedido)
-        {
+    public class PedidoRepository{
+        public void SalvarNoBanco(Pedido pedido){
             Console.WriteLine($"[SRP] Salvando pedido {pedido.Id} no banco de dados...");
         }
     }
 
-    public class RelatorioPedidoService
-    {
-        public void GerarRelatorio(Pedido pedido)
-        {
+    public class RelatorioPedidoService{
+        public void GerarRelatorio(Pedido pedido){
             Console.WriteLine($"[SRP] Gerando relatório comercial para o pedido {pedido.Id}...");
         }
     }
 
-    public class CalculadoraFreteService
-    {
-        public void CalcularFrete(Pedido pedido)
-        {
+    public class CalculadoraFreteService{
+        public void CalcularFrete(Pedido pedido){
             Console.WriteLine($"[SRP] Calculando valor logístico do pedido {pedido.Id}...");
         }
     }
