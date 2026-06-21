aa4842a1a40350f0ba837c8374226bd283b5dbb4
Phillipi Garcia <164583791+phillipi-garcia@users.noreply.github.com>
2026-06-21 12:51:42 -0300

    Update OcpClasses.cs

--- src/Logistica.App/OCP/OcpClasses.cs
+++ src/Logistica.App/OCP/OcpClasses.cs
@@ -1,29 +1,22 @@
-namespace Logistica.App.OCP
-{
-    public interface IFrete
-    {
+namespace Logistica.App.OCP{
+    public interface IFrete{
         decimal Calcular(decimal peso);
     }
 
-    public class FreteNormal : IFrete
-    {
+    public class FreteNormal : IFrete{
         public decimal Calcular(decimal peso) => peso * 2;
     }
 
-    public class FreteExpresso : IFrete
-    {
+    public class FreteExpresso : IFrete{
         public decimal Calcular(decimal peso) => peso * 5;
     }
 
-    public class FretePremium : IFrete
-    {
+    public class FretePremium : IFrete{
         public decimal Calcular(decimal peso) => peso * 8;
     }
 
-    public class CalculadoraFrete
-    {
-        public decimal Calcular(IFrete frete, decimal peso)
-        {
+    public class CalculadoraFrete{
+        public decimal Calcular(IFrete frete, decimal peso){
             return frete.Calcular(peso);
         }
     }
