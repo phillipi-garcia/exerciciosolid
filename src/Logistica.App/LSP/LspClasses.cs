dc6a523d639cda289597fa37840c889eeeb4cb71
Phillipi Garcia <164583791+phillipi-garcia@users.noreply.github.com>
2026-06-21 12:51:13 -0300

    Update LspClasses.cs

--- src/Logistica.App/LSP/LspClasses.cs
+++ src/Logistica.App/LSP/LspClasses.cs
@@ -1,34 +1,26 @@
 using System;
 
-namespace Logistica.App.LSP
-{
-    public abstract class Veiculo
-    {
+namespace Logistica.App.LSP{
+    public abstract class Veiculo{
         public string Placa { get; set; }
     }
 
-    public interface IVeiculoACombustao
-    {
+    public interface IVeiculoACombustao{
         void Abastecer();
     }
 
-    public interface IVeiculoEletrico
-    {
+    public interface IVeiculoEletrico{
         void Recarregar();
     }
 
-    public class Carro : Veiculo, IVeiculoACombustao
-    {
-        public void Abastecer()
-        {
+    public class Carro : Veiculo, IVeiculoACombustao{
+        public void Abastecer(){
             Console.WriteLine("[LSP] Abastecendo o carro com gasolina/etanol no posto.");
         }
     }
 
-    public class CarroEletrico : Veiculo, IVeiculoEletrico
-    {
-        public void Recarregar()
-        {
+    public class CarroEletrico : Veiculo, IVeiculoEletrico{
+        public void Recarregar(){
             Console.WriteLine("[LSP] Plugando o carro elétrico na estação de recarga.");
         }
     }
