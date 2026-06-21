f51414f173e8bf89c9103ef4c44dab139f02dfc2
Phillipi Garcia <164583791+phillipi-garcia@users.noreply.github.com>
2026-06-21 12:48:25 -0300

    Create LspClasses.cs

---
+++ src/Logistica.App/LSP/LspClasses.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Logistica.App.LSP
+{
+    public abstract class Veiculo
+    {
+        public string Placa { get; set; }
+    }
+
+    public interface IVeiculoACombustao
+    {
+        void Abastecer();
+    }
+
+    public interface IVeiculoEletrico
+    {
+        void Recarregar();
+    }
+
+    public class Carro : Veiculo, IVeiculoACombustao
+    {
+        public void Abastecer()
+        {
+            Console.WriteLine("[LSP] Abastecendo o carro com gasolina/etanol no posto.");
+        }
+    }
+
+    public class CarroEletrico : Veiculo, IVeiculoEletrico
+    {
+        public void Recarregar()
+        {
+            Console.WriteLine("[LSP] Plugando o carro elétrico na estação de recarga.");
+        }
+    }
+}
